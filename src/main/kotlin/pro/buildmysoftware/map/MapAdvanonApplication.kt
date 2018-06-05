3f7ed045f49240e77c35173330181d81f057b644
Mike Wojtyna <mike@buildmysoftware.pro>
2018-06-05 13:34:00 +0200

    Create project template

---
+++ src/main/kotlin/pro/buildmysoftware/map/MapAdvanonApplication.kt
@@ -0,0 +1,11 @@
+package pro.buildmysoftware.map
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MapAdvanonApplication
+
+fun main(args: Array<String>) {
+    runApplication<MapAdvanonApplication>(*args)
+}
