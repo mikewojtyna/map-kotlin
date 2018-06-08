8c405a97cd90161a058640f10299373e0c531587
Mike Wojtyna <mike@buildmysoftware.pro>
2018-06-08 22:00:32 +0200

    Add API endpoint to get all islands

--- src/main/kotlin/pro/buildmysoftware/map/MapController.kt
+++ src/main/kotlin/pro/buildmysoftware/map/MapController.kt
@@ -1,5 +1,6 @@
 package pro.buildmysoftware.map
 
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -14,8 +15,19 @@ internal class MapController(
      */
     private val appService: AppService
 ) {
+    /**
+     * Creates a new map using [appService].
+     */
     @PostMapping
     fun createMap() {
         appService.createMap()
+    }
+
+    /**
+     * Returns all islands on a map using [appService].
+     */
+    @GetMapping
+    fun islands(): Set<Island> {
+        return appService.islands()
     }
 }
--- src/test/kotlin/pro/buildmysoftware/map/MapControllerIntegrationTest.kt
+++ src/test/kotlin/pro/buildmysoftware/map/MapControllerIntegrationTest.kt
@@ -1,16 +1,18 @@
 package pro.buildmysoftware.map
 
+import org.hamcrest.Matchers.`is`
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.Mockito.times
-import org.mockito.Mockito.verify
+import org.mockito.Mockito.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @WebMvcTest
@@ -18,18 +20,47 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 internal class MapControllerIntegrationTest {
     @MockBean
     private lateinit var appService: AppService
+    @Autowired
+    private lateinit var mockMvc: MockMvc
 
     @DisplayName(
         "should create new map using app-level service when POST is " +
-                "made on '/map' endpoint"
+                "made on '/api/maps' endpoint"
     )
     @Test
-    fun createMap(@Autowired mockMvc: MockMvc) {
+    fun createMap() {
         // when
-        mockMvc.perform(post("/map"))
+        mockMvc.perform(post("/api/maps"))
 
             // then
             .andExpect(status().isOk)
         verify(appService, times(1)).createMap()
+    }
+
+    @DisplayName(
+        "should get all islands using app-level service when GET is " +
+                "made on 'api/islands'"
+    )
+    @Test
+    fun islands() {
+        // given
+        `when`(appService.islands()).thenReturn(
+            setOf(
+                island
+                    (
+                    { "island-id" },
+                    landTile(1, 2)
+                )
+            )
+        )
+
+        // when
+        mockMvc.perform(get("/api/islands"))
+            // then
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$[0].id", `is`("island-id")))
+            .andExpect(jsonPath("$[0].landTiles[0].x", `is`(1)))
+            .andExpect(jsonPath("$[0].landTiles[0].y", `is`(2)))
+        verify(appService, times(1)).islands()
     }
 }
